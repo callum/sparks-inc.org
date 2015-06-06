ad34e39587c68dbf2ee0aa48bb77fd48736b3ea4
Callum <callum@callumj.co.uk>
2015-06-06 15:56:56 +0100

    Empty bucket on deploy

--- deploy.js
+++ deploy.js
@@ -1,21 +1,25 @@
 'use strict';
 
+var path = require('path');
 var readdirp = require('readdirp');
 var s3sync = require('s3-sync');
-var path = require('path');
+var emptyBucket = require('./lib/emptyBucket');
 
-var files = readdirp({
-  root: path.join(__dirname, 'build/')
-});
-
-var uploader = s3sync({
+var config = {
   key: process.env.AWS_ACCESS_KEY,
   secret: process.env.AWS_SECRET_KEY,
   bucket: process.env.NODE_ENV + '.sparks-inc.org',
   region: 'eu-west-1'
-})
-.on('data', function (file) {
-  console.log(file.path + ' -> ' + file.url);
-});
+};
 
-files.pipe(uploader);
+emptyBucket(config, function () {
+  var files = readdirp({
+    root: path.join(__dirname, 'build/')
+  });
+
+  var uploader = s3sync(config).on('data', function (file) {
+    console.log(file.path + ' -> ' + file.url);
+  });
+
+  files.pipe(uploader);
+});
