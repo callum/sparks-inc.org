69be9fe5ca5b58a7e1bb333fcb97e37c438f8674
Callum <callum@callumj.uk>
2017-03-28 23:52:37 +0100

    Add watchmode back in

--- build.js
+++ build.js
@@ -1,10 +1,11 @@
 var metalsmith = require('metalsmith')
-var changed = require('metalsmith-changed')
 var collections = require('metalsmith-collections')
 var ignore = require('metalsmith-ignore')
 var jade = require('metalsmith-jade')
 var postcss = require('metalsmith-postcss')
 var sass = require('metalsmith-sass')
+var serve = require('metalsmith-serve')
+var watch = require('metalsmith-watch')
 var patterns = require('css-patterns')
 var config = require('./lib/plugins/config')
 var meta = require('./lib/plugins/meta')
@@ -14,18 +15,14 @@ var sassHelper = require('./lib/helpers/sass')
 
 var m = metalsmith(__dirname)
 
+var watchmode = process.argv[2] === '-w'
 var production =
   process.env.NODE_ENV === 'staging' ||
   process.env.NODE_ENV === 'production'
 
 m.destination('public')
 
-if (!production) {
-  m.clean(false)
-  m.use(changed())
-}
-
-m.use(config(process.env.NODE_ENV))
+m.use(config(process.env.NODE_ENV, watchmode))
 m.use(ignore([
   'includes/**/*',
   'layouts/**/*',
@@ -65,6 +62,22 @@ m.use(function (f, m, d) {
   })(f, m, d)
 })
 m.use(robots())
+
+if (watchmode) {
+  m.use(watch({
+    paths: {
+      'config/**/*': '**/*',
+      'src/**/*.jade': '**/*.jade',
+      'src/**/*.scss': 'scss/index.scss',
+      'src/**/*.!(jade|scss)': true
+    },
+    livereload: true
+  }))
+  m.use(serve({
+    host: '0.0.0.0',
+    port: process.env.PORT || 8000
+  }))
+}
 
 m.build(function (err) {
   if (err) throw err
