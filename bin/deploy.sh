a3f10a62cbb008e835df72448117c527a6959145
Callum <callum@callumj.uk>
2017-03-31 01:55:10 +0100

    Update deployment to enable better caching

---
+++ bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm run build
+aws s3 sync public/ $AWS_S3_BUCKET --delete --acl public-read \
+  --exclude '*.html' \
+  --exclude 'favicon.ico' \
+  --exclude 'robots.txt' \
+  --cache-control 'max-age=31536000'
+aws s3 sync public/ $AWS_S3_BUCKET --delete --acl public-read \
+  --exclude '*' \
+  --include '*.html' \
+  --include 'favicon.ico' \
+  --include 'robots.txt' \
+  --cache-control 'no-cache'
+aws cloudfront create-invalidation --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID --paths /*
--- build.js
+++ build.js
@@ -1,70 +1,76 @@
 var metalsmith = require('metalsmith')
 var collections = require('metalsmith-collections')
+var fingerprint = require('metalsmith-fingerprint-ignore')
 var ignore = require('metalsmith-ignore')
-var jade = require('metalsmith-jade')
 var postcss = require('metalsmith-postcss')
-var sass = require('metalsmith-sass')
 var serve = require('metalsmith-serve')
 var watch = require('metalsmith-watch')
 var patterns = require('css-patterns')
 var config = require('./lib/plugins/config')
+var jade = require('./lib/plugins/jade')
 var meta = require('./lib/plugins/meta')
 var robots = require('./lib/plugins/robots')
-var jadeHelper = require('./lib/helpers/jade')
-var sassHelper = require('./lib/helpers/sass')
+var sass = require('./lib/plugins/sass')
 
-var m = metalsmith(__dirname)
+var msmith = metalsmith(__dirname)
 
 var watchmode = process.argv[2] === '-w'
 var production =
   process.env.NODE_ENV === 'staging' ||
   process.env.NODE_ENV === 'production'
 
-m.destination('public')
+msmith.destination('public')
 
-m.use(config(process.env.NODE_ENV, watchmode))
-m.use(ignore([
+msmith.use(ignore([
   'includes/**/*',
   'layouts/**/*',
-  'mixins/**/*'
+  'mixins/**/*',
+  '**/.*'
 ]))
+msmith.use(config(process.env.NODE_ENV, watchmode))
+msmith.use(meta())
+msmith.use(robots())
 
-m.use(function (f, m, d) {
-  return sass({
-    outputDir: 'css',
-    outputStyle: production ? 'compressed' : 'expanded',
-    sourceMap: !production,
-    sourceMapContents: !production,
-    sourceMapEmbed: !production,
-    includePaths: patterns.includePaths,
-    functions: sassHelper(f, m, production)
-  })(f, m, d)
-})
-m.use(postcss([
+if (production) {
+  msmith.use(fingerprint({
+    pattern: [
+      '**/*.!(jade|scss)',
+      '!favicon.ico',
+      '!robots.txt'
+    ]
+  }))
+}
+
+msmith.use(sass({
+  outputDir: 'css',
+  outputStyle: production ? 'compressed' : 'expanded',
+  sourceMap: !production,
+  sourceMapContents: !production,
+  sourceMapEmbed: !production,
+  includePaths: patterns.includePaths
+}))
+msmith.use(postcss([
   require('autoprefixer'),
   require('css-mqpacker')({ sort: true }),
   require('postcss-focus')
 ]))
-m.use(collections({
+
+if (production) msmith.use(fingerprint({ pattern: ['**/*.css'] }))
+
+msmith.use(collections({
   posts: {
     pattern: 'blog/!(index).jade',
     reverse: true,
     sortBy: 'date'
   }
 }))
-
-m.use(meta(production))
-m.use(function (f, m, d) {
-  return jade({
-    pretty: true,
-    useMetadata: true,
-    locals: jadeHelper(f, m, production)
-  })(f, m, d)
-})
-m.use(robots())
+msmith.use(jade({
+  pretty: true,
+  useMetadata: true
+}))
 
 if (watchmode) {
-  m.use(watch({
+  msmith.use(watch({
     paths: {
       'config/**/*': '**/*',
       'src/**/*.jade': '**/*.jade',
@@ -73,12 +79,12 @@ if (watchmode) {
     },
     livereload: true
   }))
-  m.use(serve({
+  msmith.use(serve({
     host: '0.0.0.0',
     port: process.env.PORT || 8000
   }))
 }
 
-m.build(function (err) {
+msmith.build(function (err) {
   if (err) throw err
 })
