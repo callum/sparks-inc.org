468fa3d17f6651f197e9aae13796d59d24f0eaa4
Callum <callum@callumj.uk>
2018-06-22 11:42:37 +0100

    Add facility to redirect pages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,4 +12,12 @@ aws s3 sync public/ $AWS_S3_BUCKET --delete --acl public-read \
   --include 'favicon.ico' \
   --include 'robots.txt' \
   --cache-control 'no-cache'
+
+while IFS=' ' read -ra line || [[ -n "$line" ]]; do
+  aws s3 cp public${line[0]} $AWS_S3_BUCKET${line[0]} \
+    --acl public-read \
+    --cache-control 'no-cache' \
+    --website-redirect ${line[1]}
+done < ./redirects
+
 aws cloudfront create-invalidation --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID --paths '/*'
